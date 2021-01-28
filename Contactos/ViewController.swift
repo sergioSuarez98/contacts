8c2af0a79c245ae2704f9d1834ded18dd02474f9
Pedro <RedBoy2124@gmail.com>
2021-01-28 14:34:14 +0100

    Alertas

--- Contactos/ViewController.swift
+++ Contactos/ViewController.swift
@@ -8,14 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    @IBOutlet weak var email: underlined!
+    let alert = UIAlertController(title: "Algo salió mal", message: "Las contraseñas no coinciden", preferredStyle: .alert)
+    let alertEmail = UIAlertController(title: "Algo salió mal", message: "Email no valido", preferredStyle: .alert)
+    @IBOutlet weak var confirmarPassword: underlined!
     @IBOutlet weak var registrarseButton: UIButton!
+    @IBOutlet weak var contraseña: underlined!
     override func viewDidLoad() {
         super.viewDidLoad()
         registrarseButton.layer.cornerRadius = 7
     }
+    
+    @IBAction func validar(_ sender: Any) {
+        
+        if(isValidEmail(email.text ?? " ")){
+        if(confirmarPassword.text == contraseña.text ){
+           
+            if let secondVc = storyboard?.instantiateViewController(withIdentifier: "Second") {
+                navigationController?.pushViewController(secondVc, animated: true)
+            }
+        }else{
+            print("email no valido")
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
 
+            // 4. Present the alert.
+            self.present(alert, animated: true, completion: nil)
+        }
+        }else{
+            print("Deberia salir error de email")
+            alertEmail.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+
+            // 4. Present the alert.
+            self.present(alertEmail, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
 
 }
 
